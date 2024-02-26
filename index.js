5dd0fddbc247f4af69696be1cbb03924b97ac440
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-02-27 00:29:19 +0500

    simple-crud-completed

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const arr = [];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Get all users
+app.get('/api/v1/users', (req, res) => {
+  res.send(arr);
+});
+
+// Add user
+app.post('/api/v1/users', (req, res) => {
+  const { title } = req.body;
+
+  arr.push({
+    title: title,
+    id: Date.now(),
+  });
+
+  res.send('User successfully added');
+});
+
+
+//delete user
+app.delete('/api/v1/users/:id' , (req , res) => {
+     const {id} = req.params
+     const index = arr.findIndex((user)=> user.id === Number(id))
+
+      if(index === -1){
+      res.send('user not found')
+      return 
+      }
+    arr.splice(index , 1)
+    res.send('user deleted')
+})
+
+
+//edit user
+app.put('/api/v1/users/:id', (req , res) => {
+     const { title } = req.body
+     const { id }   =  req.params
+
+     const index = arr.findIndex((user) => user.id  === Number(id))
+
+     if (index === -1) {
+      res.send('user not found');
+      return
+     }
+
+     arr[index].title = title
+     res.send('user edited successfully')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
